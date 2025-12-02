6f361354f8ae3dbd1f7aa4fbef1ee053f50230dc
Sarthak-Bhagat2006 <sarthakbhagat2006@gmail.com>
2025-12-03 03:56:36 +0530

    Updated backend logic

--- server.js
+++ server.js
@@ -11,9 +11,23 @@ const PORT = 8080;
 
 // Middlewares
 app.use(express.json());
+import cors from 'cors';
+
+const allowedOrigins = [
+    'https://saarthi-ai-assistant-frontend.vercel.app'
+];
+
 app.use(cors({
-    origin: 'https://saarthi-ai-assistant-frontend.vercel.app/',
-    credentials: true,
+    origin: function (origin, callback) {
+        // allow requests with no origin like mobile apps or curl
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    credentials: true
 }));
 
 // Routes
