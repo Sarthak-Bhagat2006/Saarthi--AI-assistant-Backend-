3420cf45413817a44a5727d882e34073795f48ee
Sarthak-Bhagat2006 <sarthakbhagat2006@gmail.com>
2025-12-12 00:42:17 +0530

    Added health API  auto-ping solution

--- Routes/chat.js
+++ Routes/chat.js
@@ -5,7 +5,7 @@ import { authMiddleware } from "../Utils/authMiddleware.js";
 
 const router = express.Router();
 
-app.get("/health", (req, res) => {
+router.get("/health", (req, res) => {
     res.status(200).json({ status: "OK" });
 });
 
