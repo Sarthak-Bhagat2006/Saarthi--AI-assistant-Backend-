e5a863be3f5870bf315902905de12372b7b13c6b
Sarthak-Bhagat2006 <sarthakbhagat2006@gmail.com>
2026-05-26 15:59:07 +0530

    WIP- Implement reset password routes

--- Utils/authMiddleware.js
+++ Utils/authMiddleware.js
@@ -23,7 +23,7 @@ export const authMiddleware = async (req, res, next) => {
         req.user = user; // attach user to request
         next();
     } catch (error) {
-        console.log("JWT Error:", error.message); // ✅ more descriptive
+        console.log("JWT Error:", error.message); // more descriptive
         return res.status(401).json({ error: "Invalid or expired token" });
     }
 };
